22fb12c599ef9669f36e36109440f1d2ac71f541
sherlock <b417044@iiit-bh.ac.in>
2019-05-08 10:10:15 +0530

    Initial commit

---
+++ app/src/main/java/com/sherlock/notepad/notes.java
@@ -0,0 +1,43 @@
+package com.sherlock.notepad;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes_table")
+public class notes {
+    
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private int priority;
+
+    public notes(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
